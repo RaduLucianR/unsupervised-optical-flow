20f5be38a3cb4a507bb251c6b83476ddc608b10d
Jake Rap <j.e.w.rap@student.tue.nl>
2021-04-09 09:55:42 +0200

    supervised training

--- Optical Flow/FlowNetPytorch/datasets/util.py
+++ Optical Flow/FlowNetPytorch/datasets/util.py
@@ -8,13 +8,37 @@ def split2list(images, split, default_split=0.9):
         assert(len(images) == len(split_values))
     elif split is None:
         split_values = np.random.uniform(0,1,len(images)) < default_split
+        print("oh no somethings wrong")
+        exit()
     else:
         try:
             split = float(split)
         except TypeError:
             print("Invalid Split value, it must be either a filepath or a float")
             raise
-        split_values = np.random.uniform(0,1,len(images)) < split
+        # split_values = np.random.uniform(0,1,len(images)) < split
+        L = len(images)
+        split_values = [1]*round(split*L) + [0]*round((1-split)*L)
     train_samples = [sample for sample, split in zip(images, split_values) if split]
     test_samples = [sample for sample, split in zip(images, split_values) if not split]
     return train_samples, test_samples
+
+
+
+# def split2list(images, split, default_split=0.9):
+#     if isinstance(split, str):
+#         with open(split) as f:
+#             split_values = [x.strip() == '1' for x in f.readlines()]
+#         assert(len(images) == len(split_values))
+#     elif split is None:
+#         split_values = np.random.uniform(0,1,len(images)) < default_split
+#     else:
+#         try:
+#             split = float(split)
+#         except TypeError:
+#             print("Invalid Split value, it must be either a filepath or a float")
+#             raise
+#         split_values = np.random.uniform(0,1,len(images)) < split
+#     train_samples = [sample for sample, split in zip(images, split_values) if split]
+#     test_samples = [sample for sample, split in zip(images, split_values) if not split]
+#     return train_samples, test_samples
